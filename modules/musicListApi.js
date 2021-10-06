895734e3ac99ce902319fb1b873148e878298014
Ziyadhs <ziyadhs2001@gmail.com>
2021-10-06 16:41:38 +0300

    save changes

--- modules/musicListApi.js
+++ modules/musicListApi.js
@@ -3,15 +3,15 @@ let cacheMemory = {};
 
 // get music Handler fun
 function getMusicSearchHandler(req, res) {
-  let songName = req.query.songID;
+  let song = req.query.songID;
   // console.log(req.query);
 
   let musicURL = {
     method: 'GET',
-    url: 'https://shazam.p.rapidapi.com/songs/list-artist-top-tracks',
-    params: { id: songName, locale: 'en-US' },
+    url: 'https://shazam-core.p.rapidapi.com/v1/tracks/related',
+    params: { track_id: song, limit: '50' },
     headers: {
-      'x-rapidapi-host': 'shazam.p.rapidapi.com',
+      'x-rapidapi-host': 'shazam-core.p.rapidapi.com',
       'x-rapidapi-key': `${process.env.MUSIC_API_KEY}`
     }
   };
@@ -22,10 +22,10 @@ function getMusicSearchHandler(req, res) {
       // console.log("test",MusicResults.data.tracks);
       // console.log( MusicResults.data.tracks.hits);
       // console.log('inside sending request');
-      let newMusicArray = MusicResults.data.tracks.map(element => {
+      let newMusicArray = MusicResults.data.map(element => {
         return new MusicData(element);
       });
-      cacheMemory[songName] = newMusicArray;
+      cacheMemory[song] = newMusicArray;
       res.send(newMusicArray);
     });
   }
@@ -34,11 +34,30 @@ function getMusicSearchHandler(req, res) {
   }
   // console.log('after sending request');
 }
+function imageHandler (image){
+  console.log("hiii",image.images);
+if(image == null ){
+  return "https://www.liveabout.com/thmb/pwO4o_iDrZRTmmhs7eOfD25Qoqw=/1500x1125/smart/filters:no_upscale()/pop-music-57bce3863df78c87634ea806.jpg";
+}
+else if (image.images.coverart){
+  return image.images.coverart;
+}
+else if (image.images.coverarthq){
+  return image.images.coverarthq;
+}
+else if (image.images.joecolor){
+  return image.images.joecolor;
+}
+else if(image.images.background){
+  return image.images.background
+}
+
+}
 
 // music class
 class MusicData {
   constructor(element) {
-    this.img = element.images.coverart
+    this.img = element.images? imageHandler(element):"https://www.liveabout.com/thmb/pwO4o_iDrZRTmmhs7eOfD25Qoqw=/1500x1125/smart/filters:no_upscale()/pop-music-57bce3863df78c87634ea806.jpg"
     this.title = element.title;
     this.artist = element.subtitle;
     this.songURL = element.url;
